9199a1b5fb08fccc5f1a88a96052cded05ce8862
syahrulmunif <syahrulmunif@live.jp>
2016-02-27 08:54:00 +0700

    Web UI using angular@1.1X

---
+++ web/test/karma.conf.js
@@ -0,0 +1,105 @@
+// Karma configuration
+// http://karma-runner.github.io/0.12/config/configuration-file.html
+// Generated on 2016-02-27 using
+// generator-karma 1.0.0
+
+module.exports = function(config) {
+  'use strict';
+
+  config.set({
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '../',
+
+    // testing framework to use (jasmine/mocha/qunit/...)
+    // as well as any additional frameworks (requirejs/chai/sinon/...)
+    frameworks: [
+      "jasmine"
+    ],
+
+    // list of files / patterns to load in the browser
+    files: [
+      // bower:js
+      'bower_components/modernizr/modernizr.js',
+      'bower_components/jquery/dist/jquery.js',
+      'bower_components/angular/angular.js',
+      'bower_components/angular-animate/angular-animate.js',
+      'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+      'bower_components/angular-cookies/angular-cookies.js',
+      'bower_components/flexslider/jquery.flexslider.js',
+      'bower_components/angular-flexslider/angular-flexslider.js',
+      'bower_components/angular-simple-logger/dist/angular-simple-logger.js',
+      'bower_components/lodash/lodash.js',
+      'bower_components/angular-google-maps/dist/angular-google-maps.js',
+      'bower_components/angular-loading-bar/build/loading-bar.js',
+      'bower_components/angular-resource/angular-resource.js',
+      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-sanitize/angular-sanitize.js',
+      'bower_components/angular-touch/angular-touch.js',
+      'bower_components/moment/moment.js',
+      'bower_components/fullcalendar/dist/fullcalendar.js',
+      'bower_components/angular-ui-calendar/src/calendar.js',
+      'bower_components/bootstrap/dist/js/bootstrap.js',
+      'bower_components/dw-bxslider-4/jquery.bxslider.js',
+      'bower_components/fitvids/jquery.fitvids.js',
+      'bower_components/gmap3/dist/gmap3.min.js',
+      'bower_components/jquery-migrate/jquery-migrate.js',
+      'bower_components/jquery-ui/jquery-ui.js',
+      'bower_components/jquery.placeholder/jquery.placeholder.js',
+      'bower_components/jquery.stellar/jquery.stellar.js',
+      'bower_components/layerslider/js/layerslider.kreaturamedia.jquery.js',
+      'bower_components/ng-table/dist/ng-table.min.js',
+      'bower_components/underscore/underscore.js',
+      'bower_components/waypoints/lib/noframework.waypoints.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      // endbower
+      "app/scripts/**/*.js",
+      "test/mock/**/*.js",
+      "test/spec/**/*.js"
+    ],
+
+    // list of files / patterns to exclude
+    exclude: [
+    ],
+
+    // web server port
+    port: 8080,
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: [
+      "PhantomJS"
+    ],
+
+    // Which plugins to enable
+    plugins: [
+      "karma-phantomjs-launcher",
+      "karma-jasmine"
+    ],
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false,
+
+    colors: true,
+
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // Uncomment the following lines if you are using grunt's server to run the tests
+    // proxies: {
+    //   '/': 'http://localhost:9000/'
+    // },
+    // URL root prevent conflicts with the site root
+    // urlRoot: '_karma_'
+  });
+};
